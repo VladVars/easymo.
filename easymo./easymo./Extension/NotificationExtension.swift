a6872791d41819e8e2bff9ba94bbc57b87863720
VladVars <105005032+VladVars@users.noreply.github.com>
2022-07-14 21:15:56 +0300

    UserDefault

---
+++ easymo./easymo./Extension/NotificationExtension.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationExtension.swift
+//  easymo.
+//
+//  Created by mac on 14.07.22.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let loginSuccess = Notification.Name("loginSuccess")
+}
---
+++ easymo./easymo./Managers/DefaulsManager.swift
@@ -0,0 +1,22 @@
+//
+//  DefaulsManager.swift
+//  easymo.
+//
+//  Created by mac on 14.07.22.
+//
+
+import Foundation
+
+class DefaultsManager {
+    private static let defaults = UserDefaults.standard
+
+    static var loginPassword: Bool {
+        get {
+            return defaults.value(forKey: #function) as? Bool ?? false
+        }
+        set {
+            defaults.set(newValue, forKey: #function)
+        }
+    }
+    
+}
